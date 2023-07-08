71edb4a830634494b9c6f7e22f51a358e395ea19
Sergey <Melkii914@mail.ru>
2023-07-08 17:40:58 +0300

    fix: fixes after rev 12

--- src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.user.repository;
 import ru.practicum.shareit.user.model.User;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface UserRepository {
     List<User> getAllUsers();
--- src/main/java/ru/practicum/shareit/user/repository/UserRepositoryImpl.java
+++ src/main/java/ru/practicum/shareit/user/repository/UserRepositoryImpl.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.user.repository;
 
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import ru.practicum.shareit.user.model.User;
