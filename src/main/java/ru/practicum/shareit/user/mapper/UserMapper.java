39ec609084b7ae7a941e0ca8ef9a5fa9b9dd59c6
Sergey <Melkii914@mail.ru>
2023-07-09 15:13:00 +0300

    fix: ne fixes after rev 1

--- src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
@@ -1,9 +1,7 @@
 package ru.practicum.shareit.user.mapper;
 
-import ru.practicum.shareit.item.dto.ItemDto;
-import ru.practicum.shareit.item.model.Item;
-import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.model.User;
 
 public class UserMapper {
 
