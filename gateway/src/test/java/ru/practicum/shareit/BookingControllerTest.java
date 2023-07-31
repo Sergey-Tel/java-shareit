dd9c4aba6f39322e4715ed85c75a0b8b22f8befd
Sergey <Melkii914@mail.ru>
2023-07-31 12:46:44 +0300

    fix: some fixes after rev 1

--- gateway/src/test/java/ru/practicum/shareit/BookingControllerTest.java
+++ gateway/src/test/java/ru/practicum/shareit/BookingControllerTest.java
@@ -7,25 +7,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
 @AutoConfigureMockMvc
 @SpringBootTest
-class ItemControllerTest {
+class BookingControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
     @Test
-    void searchWithWrongParam() throws Exception {
-        String text = "description";
+    void getWithWrongParam() throws Exception {
 
-        mvc.perform(get("/items/search")
-                        .param("text", text)
-                        .param("from", "-1")
+        mvc.perform(get("/bookings")
+                        .param("state", "SOME")
+                        .param("from", "1")
                         .param("size", "5")
                         .header("X-Sharer-User-Id", "1"))
                 .andDo(print())
--- gateway/src/test/java/ru/practicum/shareit/ItemRequestControllerTest.java
+++ gateway/src/test/java/ru/practicum/shareit/ItemRequestControllerTest.java
@@ -1,32 +1,41 @@
 package ru.practicum.shareit;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import ru.practicum.shareit.request.dto.ItemRequestDto;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
 @AutoConfigureMockMvc
 @SpringBootTest
-class ItemControllerTest {
+class ItemRequestControllerTest {
+
+    @Autowired
+    private ObjectMapper mapper;
 
     @Autowired
     private MockMvc mvc;
 
     @Test
-    void searchWithWrongParam() throws Exception {
-        String text = "description";
-
-        mvc.perform(get("/items/search")
-                        .param("text", text)
-                        .param("from", "-1")
-                        .param("size", "5")
+    void postWithWrongParam() throws Exception {
+        ItemRequestDto request = new ItemRequestDto();
+        mvc.perform(post("/requests")
+                        .content(mapper.writeValueAsString(request))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
                         .header("X-Sharer-User-Id", "1"))
                 .andDo(print())
                 .andExpect(status().isBadRequest());
